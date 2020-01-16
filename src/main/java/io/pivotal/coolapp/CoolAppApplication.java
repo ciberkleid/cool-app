6e9d4cb27bde15e5d5611d56d89351cee8d06f56
Cora Iberkleid <ciberkleid@gmail.com>
2020-01-16 15:08:38 -0500

    first commit

---
+++ src/main/java/io/pivotal/coolapp/CoolAppApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.coolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolAppApplication.class, args);
+	}
+
+}

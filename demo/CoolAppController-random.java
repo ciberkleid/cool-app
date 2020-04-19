9e1c18193d8476099e84dff110a6b0ec325a847f
Cora Iberkleid <ciberkleid@gmail.com>
2020-04-19 18:33:35 -0400

    Dockerfiles for demos

---
+++ demo/CoolAppController-random.java
@@ -0,0 +1,26 @@
+package io.pivotal.coolapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class CoolAppController {
+
+    String[] coolSayings = {
+            "Cool beans!",
+            "Cool as a cucumber",
+            "Cool as a breeze on a hot summer day",
+            "Cool like a fool in a swimming pool"
+    };
+
+    Random random = new Random();
+
+    @RequestMapping("/")
+    public String getCoolSaying() {
+        int index = random.nextInt(coolSayings.length);
+        return coolSayings[index];
+    }
+
+}
